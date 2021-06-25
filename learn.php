ac94a7ee9a9f5ad36d909ee9839219fec13f7cd5
Thelma-Dev <osifoaisosa@gmail.com>
2021-06-25 02:04:10 +0100

    commit 1

---
+++ learn.php
@@ -0,0 +1,16 @@
+<?php
+function thelma($start, $end){
+  $procedure = $start / $end;
+  var_dump($procedure);
+}
+thelma(1000, 50). "<br>";
+ ?>
+<br>
+<?php
+function sum($first1, $first2){
+   return $first1 + $first2. "<br>";
+}
+echo sum(20, 7);
+echo sum(40, 3);
+echo sum(25, 3);
+ ?>
