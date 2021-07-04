2c5333d49d95897c6dd0cb54fd1076d27bb81427
Thelma-Dev <osifoaisosa@gmail.com>
2021-07-04 15:33:00 +0100

    Commit 3

---
+++ process.php
@@ -0,0 +1,13 @@
+<?php
+require "register.php";
+
+function Formvalidation(){
+  if (validate == true) {
+    alert("successful");
+  }
+  else {
+    alert("Not successful");
+  }
+  return validate;
+}
+?>
